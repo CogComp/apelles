0788a2c98278c9d62397d350d2319744b57a7f32
khashab2 <khashab2@illinois.edu>
2017-04-03 17:22:38 -0500

    change the port to 8080; because that's the only open one.

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname + '/public'));
 app.get('/text-viz.js', browserify('./index.js', { standalone: 'textViz' }));
 
 app.listen(3000);
-console.log('Listening at http://localhost:3000');
+console.log('Listening at http://localhost:8080');
